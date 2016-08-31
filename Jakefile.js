9534fba4dd38aa127994da336b2b7afabc31e565
evrimfeyyaz <evrimfeyyaz@gmail.com>
2016-08-31 16:11:27 +0300

    Install and use Jake for builds

---
+++ Jakefile.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Evrim Persembe on 8/31/16.
+ */
+
+(function () {
+  "use strict";
+
+  desc("Default build");
+  task("default", function() {
+    console.log("BUILD OK!");
+  });
+}());
