054521924bcad569235c266325b468a080d2e293
evrimfeyyaz <evrimfeyyaz@gmail.com>
2016-08-31 21:16:55 +0300

    Install simplebuild-jshint, create a Jake task to lint files, set JSHint settings in the package.json file, and use these settings in the IDE

--- Jakefile.js
+++ Jakefile.js
@@ -2,18 +2,23 @@
  * Created by Evrim Persembe on 8/31/16.
  */
 
+/* global desc:true, task:true, fail:true, complete:true */
+
 (function () {
   "use strict";
 
   var semver = require("semver");
+  var jshint = require("simplebuild-jshint");
 
   desc("Default build");
-  task("default", [ "node_version" ], function() {
+  task("default", [ "node_version", "lint" ], function() {
     console.log("BUILD OK!");
   });
 
   desc("Check Node version");
   task("node_version", function() {
+    console.log("Checking Node version: .");
+
     var packageJson     = require("./package.json");
     var expectedVersion = packageJson.engines.node;
     var actualVersion   = process.version;
@@ -22,4 +27,19 @@
       fail("Incorrect Node version: expected " + expectedVersion + ", but was " + process.version + ".");
     }
   });
+
+  desc("Lint the JavaScript code");
+  task("lint", function() {
+    process.stdout.write("Linting JavaScript: ");
+
+    // Get the JSHint options from package.json so
+    // we can use the same settings in JetBrains IDEs.
+    var packageJson = require("./package.json");
+
+    jshint.checkFiles({
+      files: "Jakefile.js",
+      options: packageJson.jshintConfig,
+      globals: {}
+    }, complete, fail);
+  }, { async: true });
 }());
