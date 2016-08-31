257289ec72beda82e5b0437ee5be2ace24e25958
evrimfeyyaz <evrimfeyyaz@gmail.com>
2016-08-31 17:27:54 +0300

    Install semver and add a task to check the Node version

--- Jakefile.js
+++ Jakefile.js
@@ -5,8 +5,21 @@
 (function () {
   "use strict";
 
+  var semver = require("semver");
+
   desc("Default build");
-  task("default", function() {
+  task("default", [ "node_version" ], function() {
     console.log("BUILD OK!");
+  });
+
+  desc("Check Node version");
+  task("node_version", function() {
+    var packageJson     = require("./package.json");
+    var expectedVersion = packageJson.engines.node;
+    var actualVersion   = process.version;
+
+    if (semver.neq(expectedVersion, actualVersion)) {
+      fail("Incorrect Node version: expected " + expectedVersion + ", but was " + process.version + ".");
+    }
   });
 }());
