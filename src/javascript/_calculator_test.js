88b5941556898c43a956964e09a8bba38f66e36e
Evrim P <evrimfeyyaz@gmail.com>
2016-09-02 16:01:48 +0300

    Implement rudimentary versions of basic operations

--- src/javascript/_calculator_test.js
+++ src/javascript/_calculator_test.js
@@ -8,8 +8,50 @@
   var calculator = require("./calculator.js");
 
   describe("Calculator", function() {
-    it("has an API", function() {
-      calculator.initialize();
+    describe("can do basic operations, such as", function() {
+      afterEach(function() {
+        calculator.allClear();
+      });
+
+      it("adding two numbers", function() {
+        calculator.inputOperand(5);
+        calculator.setOperator(calculator.OperatorsEnum.ADDITION);
+        calculator.inputOperand(6);
+
+        var result = calculator.compute();
+
+        assert.equal(result, 11);
+      });
+
+      it("subtracting two numbers", function() {
+        calculator.inputOperand(7);
+        calculator.setOperator(calculator.OperatorsEnum.SUBTRACTION);
+        calculator.inputOperand(10);
+
+        var result = calculator.compute();
+
+        assert.equal(result, -3);
+      });
+
+      it("multiplying two numbers", function() {
+        calculator.inputOperand(4);
+        calculator.setOperator(calculator.OperatorsEnum.MULTIPLICATION);
+        calculator.inputOperand(9);
+
+        var result = calculator.compute();
+
+        assert.equal(result, 36);
+      });
+
+      it("dividing two numbers", function() {
+        calculator.inputOperand(10);
+        calculator.setOperator(calculator.OperatorsEnum.DIVISION);
+        calculator.inputOperand(2);
+
+        var result = calculator.compute();
+
+        assert.equal(result, 5);
+      });
     });
   });
 }());
--- src/javascript/calculator.js
+++ src/javascript/calculator.js
@@ -5,7 +5,46 @@
 (function () {
   "use strict";
 
-  exports.initialize = function() {
+  var firstOperand  = null;
+  var secondOperand = null;
+  var operator      = null;
 
+  var OperatorsEnum = {
+    ADDITION: 0,
+    SUBTRACTION: 1,
+    MULTIPLICATION: 2,
+    DIVISION: 3
+  };
+  exports.OperatorsEnum = OperatorsEnum;
+
+  exports.inputOperand = function(number) {
+    if (operator === null) {
+      firstOperand = number;
+    } else {
+      secondOperand = number;
+    }
+  };
+
+  exports.setOperator = function(operatorEnum) {
+    operator = operatorEnum;
+  };
+
+  exports.compute = function() {
+    switch (operator) {
+      case OperatorsEnum.ADDITION:
+        return firstOperand + secondOperand;
+      case OperatorsEnum.SUBTRACTION:
+        return firstOperand - secondOperand;
+      case OperatorsEnum.MULTIPLICATION:
+        return firstOperand * secondOperand;
+      case OperatorsEnum.DIVISION:
+        return firstOperand / secondOperand;
+    }
+  };
+
+  exports.allClear = function() {
+    firstOperand  = null;
+    secondOperand = null;
+    operator      = null;
   };
 }());
