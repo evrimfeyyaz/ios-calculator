c150cd6c41445551a5baf2d8003c28cc290ef313
Evrim P <evrimfeyyaz@gmail.com>
2016-09-13 15:11:16 +0300

    Refactor the tests for better readability and structure

--- src/javascript/_calculator_test.js
+++ src/javascript/_calculator_test.js
@@ -10,7 +10,7 @@
   describe("Calculator", function() {
     var container;
     var displayPanel;
-    var numberButtons = [];
+    var numberButtons;
     var addButton;
     var subtractButton;
     var multiplyButton;
@@ -40,25 +40,32 @@
       calculator.terminate();
     });
 
+    // BEGIN BASIC FUNCTIONALITY TESTS
+
     it("displays '0' when first started", function() {
-      assertNewDisplayValue("0");
+      assertCurrentDisplayValue("0");
     });
 
-    it("can display pressed numberButtons", function() {
+    it("can display pressed number buttons", function() {
       for (var i = 1; i < 10; i++) {
         pressNumber(i);
       }
       pressNumber(0);
 
-      assertNewDisplayValue("1234567890");
+      assertCurrentDisplayValue("1234567890");
     });
 
     it("doesn't display a '0' in front of numbers, even if it's pressed", function() {
       pressNumber(0);
       pressNumber(1);
 
-      assertNewDisplayValue("1");
+      assertCurrentDisplayValue("1");
     });
+
+    // END BASIC FUNCTIONALITY TESTS
+
+
+    // BEGIN BASIC FOUR OPERATIONS TESTS
 
     it("can add two numbers", function() {
       pressNumber(5);
@@ -67,7 +74,7 @@
 
       pressEquals();
 
-      assertNewDisplayValue("8");
+      assertCurrentDisplayValue("8");
     });
 
     it("can subtract two numbers", function() {
@@ -77,7 +84,7 @@
 
       pressEquals();
 
-      assertNewDisplayValue("2");
+      assertCurrentDisplayValue("2");
     });
 
     it("can multiply two numbers", function() {
@@ -87,7 +94,7 @@
 
       pressEquals();
 
-      assertNewDisplayValue("15");
+      assertCurrentDisplayValue("15");
     });
 
     it("can divide two numbers", function() {
@@ -97,7 +104,7 @@
 
       pressEquals();
 
-      assertNewDisplayValue("2");
+      assertCurrentDisplayValue("2");
     });
 
     it("can combine additions and subtractions together", function() {
@@ -110,7 +117,7 @@
 
       pressEquals();
 
-      assertNewDisplayValue("12");
+      assertCurrentDisplayValue("12");
     });
 
     it("can combine multiplications and divisions together", function() {
@@ -123,8 +130,13 @@
 
       pressEquals();
 
-      assertNewDisplayValue("25");
+      assertCurrentDisplayValue("25");
     });
+
+    // END BASIC FOUR OPERATIONS TESTS
+
+
+    // BEGIN PRIORITY OPERATIONS TESTS
 
     it("can combine operands of multiple multiplications right after addition (e.g. '3+(5*6*2)')", function() {
       pressNumber(3);
@@ -137,7 +149,7 @@
 
       pressEquals();
 
-      assertNewDisplayValue("63");
+      assertCurrentDisplayValue("63");
     });
 
     it("can combine operands of multiple divisions right after subtraction (e.g. '12-(14/2/7)')", function() {
@@ -153,7 +165,7 @@
 
       pressEquals();
 
-      assertNewDisplayValue("11");
+      assertCurrentDisplayValue("11");
     });
 
     it("can combine all four operations (e.g. '3+(5*6)+2-(10/2)'", function() {
@@ -172,89 +184,7 @@
 
       pressEquals();
 
-      assertNewDisplayValue("30");
-    });
-
-    it("can change a non-priority operation to another non-priority operation when it is already set", function() {
-      pressNumber(1);
-      pressNumber(0);
-      pressAdd();
-      pressSubtract();
-      pressNumber(2);
-      pressSubtract();
-      pressAdd();
-      pressNumber(5);
-
-      pressEquals();
-
-      assertNewDisplayValue("13");
-    });
-
-    it("can change a non-priority operation to a priority operation when it is already set", function() {
-      pressNumber(1);
-      pressNumber(0);
-      pressAdd();
-      pressMultiply();
-      pressNumber(2);
-      pressAdd();
-      pressDivide();
-      pressNumber(5);
-
-      pressEquals();
-
-      assertNewDisplayValue("4");
-    });
-
-    it("can change a priority operation to non-priority operation when it is already set", function() {
-      pressNumber(8);
-      pressAdd();
-      pressNumber(5);
-      pressMultiply();
-      pressAdd();
-      pressNumber(1);
-      pressNumber(0);
-
-      pressEquals();
-
-      assertNewDisplayValue("23");
-    });
-
-    it("can change a priority operation to another priority operation when it is already set", function() {
-      pressNumber(8);
-      pressAdd();
-      pressNumber(1);
-      pressNumber(0);
-      pressMultiply();
-      pressDivide();
-      pressNumber(2);
-
-      pressEquals();
-
-      assertNewDisplayValue("13");
-    });
-
-    it("repeats the last operation when repeatedly asked for the calculated result (e.g. 5,*,2,=,= is '5*2*2')", function() {
-      pressNumber(5);
-      pressMultiply();
-      pressNumber(2);
-
-      pressEquals();
-      pressEquals();
-
-      assertNewDisplayValue("20");
-    });
-
-    it("repeats the last priority operation when repeatedly asked for the calculated result (e.g. 5,+,6,*,5,=,= is '(5+(6*5))*5", function() {
-      pressNumber(5);
-      pressAdd();
-      pressNumber(6);
-      pressMultiply();
-      pressNumber(5);
-
-      pressEquals();
-      pressEquals();
-
-      assertNewDisplayValue("175");
+      assertCurrentDisplayValue("30");
     });
 
     it("doesn't create a priority operation if the result was already calculated (e.g. 5,+,6,*,5,=,*,2,= is '(5+(6*5))*2", function() {
@@ -269,18 +199,115 @@
 
       pressEquals();
 
-      assertNewDisplayValue("70");
+      assertCurrentDisplayValue("70");
     });
 
-    it("repeats the operation on the same number when repeatedly set the same operation (e.g. 5,*,=,= is '5*5*5", function() {
+    // END PRIORITY OPERATIONS TESTS
+
+
+    // BEGIN OPERATION CHANGE TESTS
+
+    it("can change a non-priority operation to another non-priority operation when it is already set", function() {
+      pressNumber(1);
+      pressNumber(0);
+      pressAdd();
+      pressSubtract();
+      pressNumber(2);
+      pressSubtract();
+      pressAdd();
+      pressNumber(5);
+
+      pressEquals();
+
+      assertCurrentDisplayValue("13");
+    });
+
+    it("can change a non-priority operation to a priority operation when it is already set", function() {
+      pressNumber(1);
+      pressNumber(0);
+      pressAdd();
+      pressMultiply();
+      pressNumber(2);
+      pressAdd();
+      pressDivide();
+      pressNumber(5);
+
+      pressEquals();
+
+      assertCurrentDisplayValue("4");
+    });
+
+    it("can change a priority operation to a non-priority operation when it is already set", function() {
+      pressNumber(8);
+      pressAdd();
+      pressNumber(5);
+      pressMultiply();
+      pressAdd();
+      pressNumber(1);
+      pressNumber(0);
+
+      pressEquals();
+
+      assertCurrentDisplayValue("23");
+    });
+
+    it("can change a priority operation to another priority operation when it is already set", function() {
+      pressNumber(8);
+      pressAdd();
+      pressNumber(1);
+      pressNumber(0);
+      pressMultiply();
+      pressDivide();
+      pressNumber(2);
+
+      pressEquals();
+
+      assertCurrentDisplayValue("13");
+    });
+
+    // END OPERATION CHANGE TESTS
+
+
+    // BEGIN REPEATED OPERATIONS TESTS
+
+    it("repeats the last operation when asked for the calculated result after the first time (e.g. 5,*,2,=,= is '5*2*2')", function() {
+      pressNumber(5);
+      pressMultiply();
+      pressNumber(2);
+
+      pressEquals();
+      pressEquals();
+
+      assertCurrentDisplayValue("20");
+    });
+
+    it("repeats the last priority operation when asked for the calculated result after the first time (e.g. 5,+,6,*,5,=,= is '(5+(6*5))*5", function() {
+      pressNumber(5);
+      pressAdd();
+      pressNumber(6);
+      pressMultiply();
+      pressNumber(5);
+
+      pressEquals();
+      pressEquals();
+
+      assertCurrentDisplayValue("175");
+    });
+
+    it("repeats the operation on the same number when a user inputs one operand, the operation, and asks for the result (e.g. 5,*,=,= is '5*5*5", function() {
       pressNumber(5);
       pressMultiply();
 
       pressEquals();
       pressEquals();
 
-      assertNewDisplayValue("125");
+      assertCurrentDisplayValue("125");
     });
+
+    // END REPEATED OPERATIONS TESTS
+
+
+    // BEGIN OPERATION EDGE CASES TESTS
 
     it("uses 0 as the first operand if it isn't set", function() {
       pressMultiply();
@@ -290,8 +317,22 @@
 
       pressEquals();
 
-      assertNewDisplayValue("2");
+      assertCurrentDisplayValue("2");
     });
+
+    it("returns the number itself when only one operand and no operation is input", function() {
+      pressNumber(1);
+      pressNumber(0);
+
+      pressEquals();
+
+      assertCurrentDisplayValue("10");
+    });
+
+    // END OPERATION EDGE CASES TESTS
+
+
+    // BEGIN CLEAR BUTTON TESTS
 
     it("clears last operand and operation memory when asked to 'all clear'", function() {
       pressNumber(5);
@@ -304,150 +345,7 @@
 
       pressEquals();
 
-      assertNewDisplayValue("0");
-    });
-
-    it("calculates the percentage of the last operand when asked after putting both operands", function() {
-      pressNumber(1);
-      pressNumber(2);
-      pressAdd();
-      pressNumber(5);
-      pressNumber(0);
-      pressPercent();
-
-      pressEquals();
-
-      assertNewDisplayValue("18");
-    });
-
-    it("calculates the percentage of the first operand when asked after only putting the first operand and operator", function() {
-      pressNumber(1);
-      pressNumber(0);
-      pressAdd();
-      pressPercent();
-
-      pressEquals();
-
-      assertNewDisplayValue("11");
-    });
-
-    it("outputs 0 when the percentage is asked without inputting any operand or operator and asking for the result", function() {
-      pressPercent();
-
-      pressEquals();
-
-      assertNewDisplayValue("0");
-    });
-
-    it("calculates the percentage of the second operand when asked for the percentage with a third operand and a priority operator", function() {
-      pressNumber(5);
-      pressAdd();
-      pressNumber(6);
-      pressMultiply();
-      pressNumber(5);
-      pressNumber(0);
-      pressPercent();
-
-      pressEquals();
-
-      assertNewDisplayValue("23");
-    });
-
-    it("uses the second operand as the third operand too when asked for the percentage with a priority operation, but no third operand", function() {
-      pressNumber(5);
-      pressAdd();
-      pressNumber(1);
-      pressNumber(0);
-      pressMultiply();
-      pressPercent();
-
-      pressEquals();
-
-      assertNewDisplayValue("15");
-    });
-
-    it("turns the number to a percentage when there is no operation set", function() {
-      pressNumber(2);
-      pressNumber(0);
-      pressNumber(0);
-      pressPercent();
-
-      pressEquals();
-
-      assertNewDisplayValue("2");
-    });
-
-    it("returns the number itself when only one operand and no operation is input", function() {
-      pressNumber(1);
-      pressNumber(0);
-
-      pressEquals();
-
-      assertNewDisplayValue("10");
-    });
-
-    it("can create a non-integer decimal number using the dot button, and use it as an operand", function() {
-      pressNumber(5);
-      pressDecimal();
-      pressNumber(2);
-      pressNumber(5);
-      pressMultiply();
-      pressNumber(4);
-
-      pressEquals();
-
-      assertNewDisplayValue("21");
-    });
-
-    it("shows a decimal point when the user presses the decimal button", function() {
-      pressDecimal();
-
-      assertNewDisplayValue("0.");
-    });
-
-    it("ignores the decimal point if the user doesn't add any decimal places", function() {
-      pressNumber(5);
-      pressDecimal();
-      pressAdd();
-      pressNumber(2);
-
-      pressEquals();
-
-      assertNewDisplayValue("7");
-    });
-
-    it("doesn't add a second decimal point", function() {
-      pressNumber(1);
-      pressDecimal();
-      pressNumber(2);
-      pressDecimal();
-
-      assertNewDisplayValue("1.2");
-    });
-
-    it("changes the sign of the currently input number when the user presses the change sign button", function() {
-      pressNumber(5);
-      pressChangeSign();
-      pressAdd();
-      pressNumber(6);
-
-      pressEquals();
-
-      assertNewDisplayValue("1");
-    });
-
-    it("changes the sign of the result when the user presses the change sign button", function() {
-      pressNumber(5);
-      pressAdd();
-      pressNumber(6);
-      pressEquals();
-      pressChangeSign();
-      pressAdd();
-      pressNumber(6);
-
-      pressEquals();
-
-      assertNewDisplayValue("-5");
+      assertCurrentDisplayValue("0");
     });
 
     it("shows the 'all clear' button when the calculator starts", function() {
@@ -466,12 +364,12 @@
       pressNumber(5);
       pressClear();
 
-      assertNewDisplayValue("0");
+      assertCurrentDisplayValue("0");
 
       pressNumber(6);
       pressEquals();
 
-      assertNewDisplayValue("7");
+      assertCurrentDisplayValue("7");
     });
 
     it("shows the 'all clear' button after the user presses the 'clear' button", function() {
@@ -491,7 +389,7 @@
 
       pressEquals();
 
-      assertNewDisplayValue("6");
+      assertCurrentDisplayValue("6");
     });
 
     it("clears the result when the user presses the 'clear' button after the 'equals' button, but still uses it if the user presses an operation button", function() {
@@ -505,7 +403,7 @@
 
       pressEquals();
 
-      assertNewDisplayValue("8");
+      assertCurrentDisplayValue("8");
     });
 
     it("clears the result when the user presses the 'clear' button after the 'equals' button, and doesn't use the result if the user inputs another number", function() {
@@ -519,14 +417,177 @@
 
       pressEquals();
 
-      assertNewDisplayValue("4");
+      assertCurrentDisplayValue("4");
     });
+
+    it("doesn't keep adding fractional digits after the user presses the clear button", function() {
+      pressDecimal();
+      pressNumber(5);
+      pressClear();
+      pressNumber(5);
+
+      assertCurrentDisplayValue("5");
+    });
+
+    // END CLEAR BUTTON TESTS
+
+
+    // BEGIN PERCENTAGE OPERATION TESTS
+
+    it("calculates the percentage of the last operand when asked after putting both operands", function() {
+      pressNumber(1);
+      pressNumber(2);
+      pressAdd();
+      pressNumber(5);
+      pressNumber(0);
+      pressPercent();
+
+      pressEquals();
+
+      assertCurrentDisplayValue("18");
+    });
+
+    it("calculates the percentage of the first operand when asked after only putting the first operand and an operator", function() {
+      pressNumber(1);
+      pressNumber(0);
+      pressAdd();
+      pressPercent();
+
+      pressEquals();
+
+      assertCurrentDisplayValue("11");
+    });
+
+    it("outputs '0' when the user presses the percentage button without any operand or operator, and then asks for the result", function() {
+      pressPercent();
+
+      pressEquals();
+
+      assertCurrentDisplayValue("0");
+    });
+
+    it("calculates the percentage of the second operand when asked for the percentage with a third operand and a priority operator", function() {
+      pressNumber(5);
+      pressAdd();
+      pressNumber(6);
+      pressMultiply();
+      pressNumber(5);
+      pressNumber(0);
+      pressPercent();
+
+      pressEquals();
+
+      assertCurrentDisplayValue("23");
+    });
+
+    it("uses the second operand as the third operand when asked for the percentage with a priority operation, but no third operand", function() {
+      pressNumber(5);
+      pressAdd();
+      pressNumber(1);
+      pressNumber(0);
+      pressMultiply();
+      pressPercent();
+
+      pressEquals();
+
+      assertCurrentDisplayValue("15");
+    });
+
+    it("turns the number to a percentage when there is no operation set", function() {
+      pressNumber(2);
+      pressNumber(0);
+      pressNumber(0);
+      pressPercent();
+
+      pressEquals();
+
+      assertCurrentDisplayValue("2");
+    });
+
+    // END PERCENTAGE OPERATION TESTS
+
+
+    // BEGIN DECIMAL BUTTON TESTS
+
+    it("can create a non-integer decimal number using the dot button, and use it as an operand", function() {
+      pressNumber(5);
+      pressDecimal();
+      pressNumber(2);
+      pressNumber(5);
+      pressMultiply();
+      pressNumber(4);
+
+      pressEquals();
+
+      assertCurrentDisplayValue("21");
+    });
+
+    it("shows a decimal point when the user presses the decimal button", function() {
+      pressDecimal();
+
+      assertCurrentDisplayValue("0.");
+    });
+
+    it("ignores the decimal point if the user doesn't add any decimal places", function() {
+      pressNumber(5);
+      pressDecimal();
+      pressAdd();
+      pressNumber(2);
+
+      pressEquals();
+
+      assertCurrentDisplayValue("7");
+    });
+
+    it("doesn't add a second decimal point", function() {
+      pressNumber(1);
+      pressDecimal();
+      pressNumber(2);
+      pressDecimal();
+
+      assertCurrentDisplayValue("1.2");
+    });
+
+    // END DECIMAL BUTTON TESTS
+
+
+    // BEGIN CHANGE SIGN BUTTON TESTS
+
+    it("changes the sign of the currently input number when the user presses the change sign button", function() {
+      pressNumber(5);
+      pressChangeSign();
+      pressAdd();
+      pressNumber(6);
+
+      pressEquals();
+
+      assertCurrentDisplayValue("1");
+    });
+
+    it("changes the sign of the calculated result when the user presses the change sign button", function() {
+      pressNumber(5);
+      pressAdd();
+      pressNumber(6);
+      pressEquals();
+      pressChangeSign();
+      pressAdd();
+      pressNumber(6);
+
+      pressEquals();
+
+      assertCurrentDisplayValue("-5");
+    });
+
+    // END CHANGE SIGN BUTTON TESTS
+
+
+    // BEGIN DISPLAY UPDATE TESTS
 
     it("shows the last operand when the user inputs an operation", function() {
       pressNumber(5);
       pressAdd();
 
-      assertNewDisplayValue("5");
+      assertCurrentDisplayValue("5");
     });
 
     it("shows the intermediate result when the user inputs a second operation", function() {
@@ -535,7 +596,7 @@
       pressNumber(6);
       pressAdd();
 
-      assertNewDisplayValue("11");
+      assertCurrentDisplayValue("11");
     });
 
     it("shows the last operand when the user inputs a priority operation", function() {
@@ -544,7 +605,7 @@
       pressNumber(6);
       pressMultiply();
 
-      assertNewDisplayValue("6");
+      assertCurrentDisplayValue("6");
     });
 
     it("shows the intermediate result of the priority operation when the user inputs a second priority operation", function() {
@@ -555,16 +616,7 @@
       pressNumber(5);
       pressDivide();
 
-      assertNewDisplayValue(30);
-    });
-
-    it("doesn't keep adding fractional digits after the user presses the clear button", function() {
-      pressDecimal();
-      pressNumber(5);
-      pressClear();
-      pressNumber(5);
-
-      assertNewDisplayValue("5");
+      assertCurrentDisplayValue(30);
     });
 
     it("changes the sign of the operand and updates the displayed value even after the user has set an operation", function() {
@@ -572,18 +624,18 @@
       pressAdd();
       pressChangeSign();
 
-      assertNewDisplayValue("-5");
+      assertCurrentDisplayValue("-5");
     });
 
-    // TODO: Add tests for notifying the interface of the clear button functionality change.
+    // END DISPLAY UPDATE TESTS
 
-    // TODO: Refactor test names.
 
     // Helper functions
 
     function createDOMElementsForCalculator() {
       displayPanel = createElementInContainer("div");
 
+      numberButtons = [];
       for (var i = 0; i < 10; i++) {
         numberButtons[i] = createElementInContainer("button");
         numberButtons[i].dataset.value = i;
@@ -669,7 +721,7 @@
       clearButton.click();
     }
 
-    function assertNewDisplayValue(expectedNumberString) {
+    function assertCurrentDisplayValue(expectedNumberString) {
       sinon.assert.called(displayValueUpdated);
       assert.equal(displayValueUpdated.lastCall.args[0], expectedNumberString);
     }
